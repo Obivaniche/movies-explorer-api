a7c674cd65f3155098d4ecd5dfc8774f2aa1ad7b
Vladialsv Smirnov <obivaniche@yandex.ru>
2022-10-19 09:43:54 +0300

    Diploma
    middlewares/auth update

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,8 +1,12 @@
 const jwt = require('jsonwebtoken');
 const Unauthorized = require('../utils/Unauthorized');
 
+const { NODE_ENV, JWT_SECRET } = process.env;
+
 module.exports.auth = (req, res, next) => {
+
   const { authorization } = req.headers;
+
   if (!authorization || !authorization.startsWith('Bearer ')) {
     throw new Unauthorized('Необходима авторизация');
   }
@@ -11,7 +15,7 @@ module.exports.auth = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, 'some-secret-key');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
   } catch (err) {
     next(new Unauthorized('Необходима авторизация'));
     return;
